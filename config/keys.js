7f50ce870873b2c70fc1ae760cd04a6c30655ce3
Won <wonpark89@gmail.com>
2018-04-05 20:13:09 -0700

    heroku deployment

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var devKey = require("./dev");
+// import * as devKey from './dev'
 var prodKey = require("./prod");
 exports.keys = (process.env.NODE_ENV === 'production')
     ? prodKey
-    : devKey;
+    : null;
 //# sourceMappingURL=keys.js.map
