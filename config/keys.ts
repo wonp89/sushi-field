437c9ebfa81e78f53b24829fba2c0c5a8a16b927
Won <wonpark89@gmail.com>
2018-04-05 20:05:25 -0700

    heroku deployment

--- config/keys.ts
+++ config/keys.ts
@@ -1,6 +1,6 @@
-import * as devKey from './dev'
+// import * as devKey from './dev'
 import * as prodKey from './prod'
 
 export const keys: any = (process.env.NODE_ENV === 'production')
 ? prodKey as any
-: devKey as any
+: null
