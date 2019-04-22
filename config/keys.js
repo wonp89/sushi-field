079aba055f3158a24f2fddf7b127755f74dfb49e
Won Teak Park <wonpark@Wons-Air.hitronhub.home>
2019-04-21 20:22:29 -0700

    opening days revised

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// import * as devKey from './dev'
+var devKey = require("./dev");
 var prodKey = require("./prod");
 exports.keys = (process.env.NODE_ENV === 'production')
     ? prodKey
-    // : devKey as any
-    : null;
+    : devKey;
+// : null
 //# sourceMappingURL=keys.js.map
--- server.ts
+++ server.ts
@@ -14,46 +14,46 @@ import categoryRouter from './server/routes/categoryRouter';
 
 class Server {
 
-    public app: express.Application;
+  public app: express.Application;
 
-    constructor() {
-        this.app = express();
-        this.config();
-        this.mongooseConnect();
-        this.routes();
+  constructor() {
+    this.app = express();
+    this.config();
+    this.mongooseConnect();
+    this.routes();
+  }
+
+  public mongooseConnect(): void {
+    // const MONGO_URI: string = 'mongodb://localhost:27017/sushi-field';
+    mongoose.connect(keys.mongoURI, (err) => {
+      if (err) {
+        console.log(err.message);
+      }
+      else {
+        console.log('Connected to MongoDb');
+      }
+    });
+
+  }
+
+  public config(): void {
+
+    this.app.use(logger('dev'));
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.use(cookieParser());
+    this.app.use(express.static(path.join(__dirname, '/dist')));
+    this.app.get('*'), (req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, 'dist/index.html'))
     }
+  }
 
-    public mongooseConnect(): void {
-        // const MONGO_URI: string = 'mongodb://localhost:27017/sushi-field';
-        mongoose.connect(keys.mongoURI, (err) => {
-            if (err) {
-                console.log(err.message);
-            }
-            else {
-                console.log('Connected to MongoDb');
-            }
-        });
-
-    }
-
-    public config(): void {
-
-        this.app.use(logger('dev'));
-        this.app.use(bodyParser.urlencoded({ extended: true }));
-        this.app.use(bodyParser.json());
-        this.app.use(cookieParser());
-        this.app.use(express.static(path.join(__dirname, '/dist')));
-        this.app.get('*'), (req: Request, res: Response) => {
-            res.sendFile(path.join(__dirname, 'dist/index.html'))
-        }
-    }
-
-    public routes(): void {
-        const router: express.Router = express.Router();
-        this.app.use('/user', userRouter);
-        this.app.use('/menu', menuRouter);
-        this.app.use('/category', categoryRouter);
-    }
+  public routes(): void {
+    const router: express.Router = express.Router();
+    this.app.use('/user', userRouter);
+    this.app.use('/menu', menuRouter);
+    this.app.use('/category', categoryRouter);
+  }
 }
 
 export default new Server().app;
